47c2656d8d6da02ca7efc17f4bd468326eb05d8b
prashant jha <prashantvatsjha@gmail.com>
2020-07-21 18:52:21 +0530

    added unit test

---
+++ spring-hello-world-example/src/test/java/com/prashant/study/test/AppTest.java
@@ -0,0 +1,14 @@
+package com.prashant.study.test;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class AppTest {
+	
+	@Test
+	public void test() {
+		Assert.assertEquals(1, 1);
+	}
+
+}
